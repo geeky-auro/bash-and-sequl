84eafd4953367eeade15c629d5753d174a7785ef
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-04 19:06:39 +0530

    Upto Level 2 Question 3

---
+++ bash-basics/fork2.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+
+int main(){
+    int ret=0;
+    // if(ret==0){
+    //     printf("Wallah Its is Child`` Process \n");
+    // }
+    // if(ret>0){
+    //     printf("Wallah Its is Parent Process \n");
+    // }
+    // ret=fork();
+    printf("No of Child to be produced \n");
+    for(int i=0;i<1;i++){
+        ret=fork();
+        // print no. of child
+        int n;
+        scanf("%d",&n);
+        for(int j=0;j<n;j++){
+            ret=fork();
+            if(ret==0){
+            printf("Index:%d Parent Process ID:%d , Child Process ID:%d \n",j , getppid(),getpid());
+            break;
+            }
+        }
+    }
+
+
+
+    // if(ret>0){
+    //     int ret1=fork();
+    //     if(ret1>0){
+    //         wait(NULL);
+    //         int ret2=fork();
+    //         if(ret2>0){
+    //             wait(NULL);
+    //             printf("Parent Process:\n");
+    //             printf("PID:%d \n",getpid());
+    //             printf("PPID:%d \n",getppid());
+    //             printf("\n");
+    //         }else if(ret2==0){
+    //             printf("Child Process 1 ID:\n");
+    //             printf("PID:%d \n",getpid());
+    //             printf("PPID:%d \n",getppid());
+    //             printf("\n");
+    //         }
+    //     }else if(ret1==0){
+    //             printf("Child Process 2 ID:\n");
+    //             printf("PID:%d \n",getpid());
+    //             printf("PPID:%d \n",getppid());
+    //             printf("\n");
+    //     }
+    // }else if(ret==0){
+    //             printf("Child Process 3 ID:\n");
+    //             printf("PID:%d \n",getpid());
+    //             printf("PPID:%d \n",getppid());
+    //             printf("\n");
+    // }
+return 0;
+}
