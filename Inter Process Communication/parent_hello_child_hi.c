2f33035e9aa44d660eb2311688e232e2e84213df
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-03-23 17:37:49 +0530

    updated

--- Inter Process Communication/parent_hello_child_hi.c
+++ Inter Process Communication/parent_hello_child_hi.c
@@ -12,16 +12,28 @@ int main(){
         return(0);
     }
     // Create a Child Process
-    char readmessage[20];
+    char buffer[20];
+    char readmessage1[20];
     int ret=fork();
     if(ret==0){
         // Child Process
+        close(pipe_fd[1]);
+        close(pipe_fd1[0]);
         printf("Reading Message from Parent \n");
-        read(pipe_fd[0],readmessage,sizeof(readmessage));
-        printf("Message from Parent is %s\n",readmessage);
+        read(pipe_fd[0],buffer,sizeof(buffer));
+        printf("Message from Parent is %s\n",buffer);
+        printf("Writing Message to Parent");
+        write(pipe_fd1[1],"Hi",2);
+
     }else{
+        close(pipe_fd[0]);
+        close(pipe_fd1[1]);
         printf("Writing Message to Child \n");
         write(pipe_fd[1],"Hello",5);
+        printf("Reading Message from Child \n");
+        read(pipe_fd1[0],readmessage1,sizeof(readmessage1));
+        printf("Message from Child is %s \n",readmessage1);
+
     }
     
     return 0;
