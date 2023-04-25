c0b369472fea40647982c28256c8f1563480028a
Auro Saswat Raj <83534307+geeky-auro@users.noreply.github.com>
2023-04-25 21:01:27 +0530

    Thread Synchronization

---
+++ Inter Process Communication/Thread_Synchronizatrion/thread1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+
+int sharedVariable=0;
+pthread_mutex_t s;
+void * increment(){
+    // pthread_mutex_lock(&s);
+    for(int i = 0; i < 1000000; i++){
+        pthread_mutex_lock(&s);
+        sharedVariable++;
+        pthread_mutex_unlock(&s);
+        printf("%d \n",sharedVariable);
+        
+    }
+    
+}
+
+
+int main(){
+
+    pthread_t thread[2];
+     pthread_mutex_init(&s,NULL);
+    for(int i = 0; i < 2; i++) {
+        // Create a new thread
+    pthread_create(&thread[i], NULL,increment,NULL);
+    }
+    for(int i = 0; i < 2; i++) {
+        // Process Wait for the completion of thread ;) 
+
+    pthread_join(thread[i], NULL); 
+
+    }
+     pthread_mutex_destroy(&s);
+     printf("%d \n",sharedVariable);
+    return 0;
+}
+
+
